1f03bbf996078d6cdea83820e39bda6fc9e37e7f
VictorHugoDC1999 <victorhugodelciampo@gmail.com>
2022-05-04 18:19:37 +0000

    Nlw Stage 2

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
+import { FeedbackTypeStep } from './Steps/FeedBackTypeStep';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto'
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  OTHER: {
+    title: 'Outros',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de um balão de pensamento'
+    }
+  }
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      
+
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep feedbackType={feedbackType} />
+      )}
+
+      <footer>
+        Feito com 🖤 por <a className="underline underline-offset-2">Victor</a>
+      </footer>
+    </div>
+  );
+}
